e474dd7536a7bfc764b49573fee18c51383790e5
404b0tnet <jsmith728.js@gmail.com>
2021-09-21 11:32:33 -0400

    testing push

--- src/formatted.rs
+++ src/formatted.rs
@@ -8,6 +8,7 @@ print!: same as format! but the text is printed to the console (io::stdout).
 println!: same as print! but a newline is appended.
 eprint!: same as format! but the text is printed to the standard error (io::stderr).
 eprintln!: same as eprint!but a newline is appended.
+
 */
 
 use std::{
